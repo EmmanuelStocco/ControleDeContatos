7d63399ff47965a4f9b187c76c7c8ae311fb3d17
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-09-12 23:05:54 -0300

    chore: add cdn and configs datatables

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,9 +1,12 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
-
 // Write your JavaScript code.
 
-// jquery
+// jquery  
+$(document).ready(function () {
+    $('#pagination_example').DataTable();
+});
+ 
 $('.close-alert').click(function () {
     $('.alert').hide('hide');
 });
