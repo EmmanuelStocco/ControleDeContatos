956b8c9b5827dde9464ec08cb01bf14d78a48fd3
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-09-19 22:24:28 -0300

    feat: sessions helper

---
+++ Helper/Sessao.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using ControleDeContatos.Models; 
+using Newtonsoft.Json;
+
+namespace ControleDeContatos.Helper
+{
+    public class Sessao : ISessao
+    {
+        private readonly IHttpContextAccessor _httpContext;
+        public Sessao(IHttpContextAccessor httpContextAcessor)
+        {
+            _httpContext = httpContextAcessor;
+        }
+
+        public UsuarioModel BuscarSessaoDoUsuario()
+        {
+            string sessaoUsuario = _httpContext.HttpContext.Session.GetString("sessaoUsuarioLogado");
+            if (string.IsNullOrEmpty(sessaoUsuario)) return null;
+
+            return JsonConvert.DeserializeObject<UsuarioModel>(sessaoUsuario);   
+        }
+
+        public void CriarSessaoDoUsuario(UsuarioModel usuario)
+        {
+            string valor = JsonConvert.SerializeObject(usuario);
+            _httpContext.HttpContext.Session.SetString("sessaoUsuarioLogado", valor);
+        }
+
+        public void RemoverSessaoUsuario()
+        {
+            _httpContext.HttpContext.Session.Remove("sessaoUsuarioLogado");
+        }
+    }
+}
