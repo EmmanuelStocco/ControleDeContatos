6f3220c4283ffee025c77513511be7cd862c77b5
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-09-26 22:45:36 -0300

    feat: Model and View AlterarSenha

---
+++ ControleDeContatos/Controllers/AlterarSenhaController.cs
@@ -0,0 +1,32 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ControleDeContatos.Controllers
+{
+    public class AlterarSenhaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Alterar(AlterarSenhaModel alterarSenhaModel)
+        {
+            try
+            {
+                if(ModelState.IsValid)
+                {
+                    TempData["MensagemSucesso"] = "Senha alterada com sucesso";
+                    return View("Index", alterarSenhaModel);
+                }
+                return View("Index", alterarSenhaModel);
+
+            } catch(Exception erro)
+            {
+                TempData["MensagemErro"] = $"Ops, não conseguimos alterar sua senha, detalhe do erro: {erro.Message}";
+                return View("Index", alterarSenhaModel);
+            }
+        }
+    }
+}
