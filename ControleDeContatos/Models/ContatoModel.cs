7c81f0913a21746d834ce08d2bd838f502d00ad8
EmmanunelStocco <emmanuelrolimstocco@gmail.com>
2023-09-27 23:01:07 -0300

    feat: link and relationship between accounts and users

--- ControleDeContatos/Models/ContatoModel.cs
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -17,6 +17,8 @@ namespace ControleDeContatos.Models
         [Phone(ErrorMessage = "O celular informado não é valido")]
         public string Celular { get; set; }
 
+        public int? UsuarioId { get; set; }
 
-    }
+        public UsuarioModel? Usuario { get; set; }
+     }
 }
--- ControleDeContatos/Models/UsuarioModel.cs
+++ ControleDeContatos/Models/UsuarioModel.cs
@@ -26,6 +26,11 @@ namespace ControleDeContatos.Models
         public string Senha { get; set; }
         public DateTime DataCadastro { get; set; }
         public DateTime? DataAtualizacao { get; set; }
+
+        public virtual List<ContatoModel> Contatos { get; set; }
+
+
+
         public bool SenhaValida(string senha)
         {
             return Senha == senha.GerarHash();
